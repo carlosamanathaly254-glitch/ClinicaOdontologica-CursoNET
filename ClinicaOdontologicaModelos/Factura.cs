db52e469fb7989caaaafa7fc4ab708e4a9abed17
Nathaly Carlosama <carlosamanathaly254@gmail.com>
2026-09-23 18:24:42 -0500

    ultimaModificacion

---
+++ ClinicaOdontologicaModelos/Factura.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;       // <-- Añadido
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicaOdontologicaModelos
+{
+    [Table("Facturas")]
+    public class Factura
+    {
+        [Key]
+        [Column("id_factura")]
+        public int IdFactura { get; set; }
+
+        [Column("fecha_emision")]
+        [Required]
+        public DateTime FechaFactura { get; set; }
+
+        [Column("subtotal", TypeName = "decimal(10,2)")]
+        [Required]
+        public decimal Subtotal { get; set; }
+
+        [Column("impuesto", TypeName = "decimal(10,2)")]
+        [Required]
+        public decimal Impuesto { get; set; }
+
+        [Column("total", TypeName = "decimal(10,2)")]
+        [Required]
+        public decimal Total { get; set; }
+
+        [Column("estado_pago")]
+        [MaxLength(20)]
+        [Required]
+        public string EstadoPago { get; set; }
+
+
+        [ForeignKey("cita")]
+        [Column("id_cita")]
+        public int IdCita { get; set; }
+        public Cita Cita { get; set; }
+    }
+}
---
+++ ClinicaOdontologicaModelos/Receta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;          // <- añadir
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicaOdontologicaModelos
+{
+    [Table("Recetas")]
+    public class Receta
+    {
+        [Key]
+        [Column("id_receta")]
+        public int IdReceta { get; set; }
+
+        [Column("fecha_emision")]
+        [Required]
+        public DateTime FechaEmision { get; set; }
+
+        [Column("indicaciones")]
+        [Required]
+        public string Indicaciones { get; set; }
+
+        [ForeignKey("cita")]
+        [Column("id_cita")]
+        public int IdCita { get; set; }
+        public Cita Cita { get; set; }
+    }
+}
