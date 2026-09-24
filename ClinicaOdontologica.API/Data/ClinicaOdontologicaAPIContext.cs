a98f42df3232b36268fde5018e8df7a9e1243208
Nathaly Carlosama <carlosamanathaly254@gmail.com>
2026-09-24 18:09:11 -0500

    Modificaciones Clase 3

---
+++ ClinicaOdontologica.API/Data/ClinicaOdontologicaAPIContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ClinicaOdontologicaAPIContext(DbContextOptions<ClinicaOdontologicaAPIContext> options) : DbContext(options)
+{
+    public DbSet<ClinicaOdontologicaModelos.Cita> Citas { get; set; } = default!;
+    public DbSet<ClinicaOdontologicaModelos.Consultorio> Consultorios { get; set; } = default!;
+    public DbSet<ClinicaOdontologicaModelos.DetalleCita> DetalleCitas { get; set; } = default!;
+    public DbSet<ClinicaOdontologicaModelos.Especialidad> Especialidades { get; set; } = default!;
+    public DbSet<ClinicaOdontologicaModelos.Factura> Facturas { get; set; } = default!;
+    public DbSet<ClinicaOdontologicaModelos.HistorialMedico> HistorialMedicos { get; set; } = default!;
+    public DbSet<ClinicaOdontologicaModelos.Odontologo> Odontologos { get; set; } = default!;
+    public DbSet<ClinicaOdontologicaModelos.Paciente> Pacientes { get; set; } = default!;
+    public DbSet<ClinicaOdontologicaModelos.Receta> Recetas { get; set; } = default!;
+
+    public DbSet<ClinicaOdontologicaModelos.Tratamiento> Tratamientos { get; set; } = default!;
+
+}
